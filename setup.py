8c4116693d03c28d16dcd651e502e7e8a39959e9
Justin Reese <justaddcoffee@gmail.com>
2022-07-12 10:48:15 -0400

    Add setup.py for project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='oaksim',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Knowledge-Graph-Hub/oaksim',
+    license='BSD3',
+    author='Justin Reese',
+    author_email='Justin Reese',
+    description=''
+)
