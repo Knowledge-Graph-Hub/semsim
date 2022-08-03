d48325a4edc47802c1bcadc62d1d2792eeac763f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-03 22:11:44 +0200

    Formatted documents

--- semsim/compute_hpo_similarities.py
+++ semsim/compute_hpo_similarities.py
@@ -1,14 +1,15 @@
 from grape.datasets.kgobo import HP
-from  collections import Counter
+from collections import Counter
 import pandas as pd
 from .compute_pairwise_similarities import compute_pairwise_ancestors_jaccard, compute_pairwise_resnik
+
 
 def compute_hpo_similarities(
     resnik_path: str,
     ancestors_jaccard_path: str
 ):
     """Computes and stores similarities to the provided paths.
-    
+
     Parameters
     -----------------------
     resnik_path: str
@@ -20,10 +21,10 @@ def compute_hpo_similarities(
         .filter_from_names(
             edge_type_names_to_keep=["biolink:subclass_of"],
             node_prefixes_to_keep=["HP:"]
-        )\
+    )\
         .to_transposed()\
         .remove_disconnected_nodes()
-    
+
     counts = dict(Counter(
         pd.read_csv(
             "http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa",
