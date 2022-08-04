cca7d41af78b0b2b925e99767fa6b21d43a719f2
Justin Reese <justaddcoffee@gmail.com>
2022-08-04 12:39:36 -0400

    Add docstrings

--- semsim/run.py
+++ semsim/run.py
@@ -12,12 +12,14 @@ def cli():
 @click.option("--output-dir", "-o", required=False, default="data/raw")
 @click.argument(
     "ontology",
-    help="""An OBO Foundry ontologiy to run over [HP]""",
     default="HP"
 )
-def run(ontology, output_dir, **kwargs) -> None:
-    """Run code to produce all by all semantic similarity for a ontology [HPO].
+def run(ontology: str, output_dir: str) -> None:
+    """Generate a file containing the semantic similarity (Resnik and
+    Jaccard) for each pair of terms in an ontology (by default, ontology is HP)
 
+    :param ontology: An OBO Foundry ontology on which to compute sem sim [HP]
+    :param output_dir: Path to write out file with all by all sem sim measurements
     :return: None
     """
     print(f"ontology is {ontology}")
@@ -25,4 +27,18 @@ def run(ontology, output_dir, **kwargs) -> None:
 
 
 if __name__ == "__main__":
-    cli()
+    run()
+
+
+#     """Generate a file containing all by all 
+#     semantic similarity for each pair of terms in an ontology
+#     (by default, ontology is HP)
+
+#     :param ontology: [An OBO Foundry ontologiy to run over [HP]""",
+# ]
+#     :type ontology: [type]
+#     :param output_dir: [description]
+#     :type output_dir: [type]
+#     :return: [description]
+#     :rtype: [type]
+#     """
